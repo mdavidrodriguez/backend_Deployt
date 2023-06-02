8b3b6fa2dca6faabfd4fbe17d039148915f900fe
Mateo Rodriguez <mdavidrodriguez@unicesar.edu.co>
2023-06-02 12:29:51 -0500

    Enviando cambios

---
+++ src/database/sequelize.config.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize("tic-tac-toe-api", "apiuser", "password", {
+//   host: "192.168.1.12",
+//   dialect: "mysql",
+// });
+const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
+  host: process.env.DBHOST,
+  dialect: "mysql",
+});
+
+module.exports = { sequelize };
